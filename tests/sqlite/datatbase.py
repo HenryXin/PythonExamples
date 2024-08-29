df7781255a83253b2dfa7a4b7bbcbd1a2ffb46a8
henry <henry@ubuntu20test.myguest.virtualbox.org>
2024-08-29 11:08:19 -0400

    sqlite create table

--- tests/sqlite/datatbase.py
+++ tests/sqlite/datatbase.py
@@ -1,5 +1,28 @@
 import sqlite3
 
+# Connect to database
 #conn = sqlite3.connect(':memory:')
 conn = sqlite3.connect('customer.db')
 
+# Create a cursor
+c = conn.cursor()
+
+# Create a table
+c.execute("""CREATE TABLE customers (
+   first_name text,
+   last_name text,
+   email text
+   )
+""")
+
+# NULL
+# INTEGER
+# REAL
+# TEXT
+# BLOG
+
+# Commit
+conn.commit()
+
+# close our connection
+conn.close()
